08d26f728b2634680b71b8bf8bc250d48bac0b9b
Chaitanya Lakhchaura <90706514+ZenithFlux@users.noreply.github.com>
2024-11-28 10:31:08 +0530

    Added 'Toggle readonly' keymap
    Removed <leader>q keymap
    Added a keymap to "yank to system clipboard"

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -7,8 +7,7 @@ vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostics under the cursor' })
 ----------------------
 
 vim.keymap.set('', '<C-D>', '<C-D>zz', { desc = 'Scroll half page down and center cursor' })
@@ -25,19 +24,24 @@ vim.keymap.set('n', 'gp', '`[v`]', { desc = 'Select last [P]asted text' })
 vim.keymap.set('n', '<leader>f', vim.cmd.Ex, { desc = 'Open [F]iletree' })
 
 vim.keymap.set('v', '<leader>p', '"_dP', { desc = '[P]aste without yanking' })
+vim.keymap.set({ 'n', 'v' }, '<leader>y', '"+y', { desc = "[Y]ank to system clipboard" })
 vim.keymap.set('v', 'r', '"ry:%s~<C-R>r~~gc<Left><Left><Left>', { desc = '[R]eplace Selected Text' })
 vim.keymap.set('v', '<', '<gv', { desc = 'Outdent without exiting visual mode' })
 vim.keymap.set('v', '>', '>gv', { desc = 'Indent without exiting visual mode' })
 
 vim.keymap.set('t', '<Esc>', '<C-\\><C-N>', { desc = 'Exit Terminal Mode' })
 
-vim.keymap.set('n', '<leader>ti', function()
-  vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({nil}))
-end, { desc = "Toggle inlay hints" })
-
 vim.keymap.set('n', '<leader>th', function()
   vim.o.hlsearch = not vim.o.hlsearch
 end, { desc = 'Toggle hlsearch' })
+
+vim.keymap.set('n', '<leader>tr', function()
+  vim.bo.readonly = not vim.bo.readonly
+end, { desc = 'Toggle readonly' })
+
+vim.keymap.set('n', '<leader>ti', function()
+  vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({nil}))
+end, { desc = "Toggle inlay hints" })
 
 vim.keymap.set('n', '<leader>tx', function()
   vim.diagnostic.enable(not vim.diagnostic.is_enabled())
